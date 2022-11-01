5eb787701a99e4751be8ea9664c18e43349825ed
Don "Quixote" Eisele <quixote@toysmakeuspowerful.com>
2022-11-01 15:13:44 -0500

    initial working (from tamper) version

--- kickstarter.user.js
+++ kickstarter.user.js
@@ -11,11 +11,22 @@
 var loadnotescounter = 0;
 function loadnotesloop() {
     setTimeout(function() {
-	a = $(".backing-plus-btn:visible:not(.notespulled)");
-	a.slice(0,1).each(function(b) { $(this).click(); $(this).addClass("notespulled"); });
-	$(".backings-info__notes:has(p):not(.notesshown)").each(function(j){ $("tr#backing_"+$(this).parents("div[data-backing_id]").attr("data-backing_id")).children().eq(6).append($(this).contents().filter("p").html()); $(this).addClass("notesshown"); });
-	$(".modal_dialog_close:visible").click();
 	loadnotescounter++;
+	console.log("timing loadnotesloop");
+	$(".modal_dialog_close:visible").click();
+	let a = $(".backing-plus-btn:visible:not(.notespulled)");
+	a.slice(0,1).each(function(b) {
+	    console.log("clicking: "+$(this).parents("tr").attr("id"));
+	    $(this)[0].click();
+	    $(this).addClass("notespulled");
+	});
+	$(".backings-info__notes:has(p):not(.notesshown)").each(function(j){
+	    console.log("showing: "+$(this).parents("div[data-backing_id]").attr("data-backing_id"));
+	    $("tr#backing_"+$(this).parents("div[data-backing_id]").attr("data-backing_id")).children().eq(6).append($(this).contents().filter("p").html());
+	    $(this).addClass("notesshown");
+	});
+	$(".modal_dialog_close:visible").click();
+	// I have a ton of pledges... this number might change for people cloning this.
 	if(loadnotescounter<130) {
 	    loadnotesloop();
 	}
@@ -33,15 +44,22 @@ $(document).ready(function() {
 		$(":checkbox[id^=backer_completed_at][value=true]").closest("tr").hide();
 		$('button[data-sentiment="negative"][data-selected="true"]').closest('tr').hide();
 		$('#uncollected').hide();
-		console.log($("#collected a.show_more_backings"));
+		//console.log($("#collected a.show_more_backings"));
 		if($("#collected a.show_more_backings").hasClass("loading")) {
+		    console.log("still loading");
 		    return;
 		}
 		if($("#collected a.show_more_backings").is(":hidden")) {
+		    //console.log("show more hidden now");
+		    if(loadnotescounter<1) {
+			console.log("triggering loadnotesloop");
+			loadnotescounter++;
+			loadnotesloop();
+		    }
 		    return;
-		}
+		}				  
 		$("#collected a.show_more_backings")[0].click();
-	    }				  
+	    }
 	});    
     });
     var config = { 
@@ -49,11 +67,9 @@ $(document).ready(function() {
 	childList: true, 
 	characterData: true 
     };
-    observer.observe($("#collected table tbody")[0], config);
+    observer.observe($("#collected table")[0], config);
     // observer.disconnect();
     $("#collected a.show_more_backings")[0].click();
-
-    loadnotesloop();
 
     $(".backings-info__notes:not(:has(p))").each(function(j) { $("tr#backing_"+$(this).parents("div[data-backing_id]").attr("data-backing_id")).find(".backing-plus-btn:visible").removeClass("notespulled"); $(this).removeClass("notesshown"); });
 });
